e1030a2310e421d9a0a9d24a6b1b0aa74aa94c7c
zadi15 <44522755+zadi15@users.noreply.github.com>
2020-07-03 00:02:29 +0100

    Added first commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # explosionCancel
 A plugin to stop all explosions from affecting blocks (1.16.1 only)
+
+More updates to follow soon.
---
+++ src/zadi15/explosionCancel/Main.java
@@ -0,0 +1,43 @@
+package zadi15.explosionCancel;
+
+/*  
+ *  Copyright https://github.com/zadi15
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	public void onDisable() {
+	}
+
+	@EventHandler
+	public void onDeath(BlockExplodeEvent event) throws InterruptedException {
+		event.setCancelled(true);
+		return;
+	}
+	
+	@EventHandler
+	public void onDeath(EntityExplodeEvent event2) throws InterruptedException {
+		event2.setCancelled(true);
+		return;
+	}
+}
